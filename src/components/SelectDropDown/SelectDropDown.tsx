36b855555e94b496808f899f11e9477401af86c9
Divya P <panackal.divya@gmail.com>
2022-08-04 23:16:58 +0530

    select ui

--- src/components/SelectDropDown/SelectDropDown.tsx
+++ src/components/SelectDropDown/SelectDropDown.tsx
@@ -4,7 +4,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 import './SelectDropDown.scss';
 
@@ -20,13 +20,16 @@ export default function SelectDropDown(props: any) {
     <div className="select-wrapper">
     <Box sx={{ minWidth: 120 }} className="select-wrapper--box">
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Your answer</InputLabel>
+        {/* <InputLabel id="demo-simple-select-label">Your answer</InputLabel> */}
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           value={age}
           label="Your answer"
           onChange={handleChange}
+          IconComponent = {KeyboardArrowDownIcon}
+          style={{ width: '150px', color: '#98C93C', borderColor: '#98C93C'}}
+          className="simple-select-dropdown"
         >
         {props.values.answers.map((option: any, index: any) => {
            return <MenuItem value={option.content} key={index} id={option.id}>{option.content}</MenuItem>
