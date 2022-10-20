a48d4ce9ed005705631aaead975db281e56a40ee
Divya P <panackal.divya@gmail.com>
2022-10-20 21:58:35 +0530

    emoji popup

--- src/components/shared/StudentDashboard.tsx
+++ src/components/shared/StudentDashboard.tsx
@@ -3,8 +3,26 @@ import { Container, Box, AppBar } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import '../../App.scss';
 import bgImg from 'assets/student-logo.svg';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import Popover from '@mui/material/Popover';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
 
 export default function StudentDashboard(props: any) {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
     <Box className='dashboard-bg'>
         <img alt="" className="dashboard-bg-img" src={bgImg} />
@@ -15,8 +33,42 @@ export default function StudentDashboard(props: any) {
         >
            
           <div className='text-center'>
-            <h2 className="mb-3 pt-5">Student Dashboard</h2>
+            <h2 className="mb-3 pt-5">Student Dashboard <span></span></h2>
             <p>Placement Test</p>
+
+            
+            {/* Emoji Popup starts */}
+            <Button aria-describedby={id} variant="contained" onClick={handleClick}>
+            <EmojiEmotionsIcon />
+            </Button>
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+            >
+              <div className='p-2 d-flex flex-wrap emoji-box'>
+                <Button variant="text" >😀</Button>
+                <Button variant="text" >😁</Button>
+                <Button variant="text" >😂</Button>
+                <Button variant="text" >😃</Button>
+                <Button variant="text" >😄</Button>
+                <Button variant="text" >😅</Button>
+                <Button variant="text" >😆</Button>
+                <Button variant="text" >😇</Button>
+                <Button variant="text" >😈</Button>
+                <Button variant="text" >😉</Button>
+                <Button variant="text" >😊</Button>
+                <Button variant="text" >😋</Button>                
+              </div>
+            </Popover>
+            {/* Emoji Popup ends */}
+
+
           </div>
         </Box>
       </Container>
